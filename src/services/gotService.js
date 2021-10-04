6ebd34c7986dc65391716b39c6a307ea9ea6eb81
Viktoria <vika.perova.88.88@mail.ru>
2021-10-04 11:22:47 +0300

    gotService 60str

---
+++ src/services/gotService.js
@@ -0,0 +1,61 @@
+
+export default class GotService {
+    constructor() {
+        this._apiBase = 'https://www.anapioficeandfire.com/api';
+    }
+
+    getResource = async (url) => {
+        const res = await fetch(`${this._apiBase}${url}`);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}` +
+                `, received ${res.status}`);
+        }
+        return await res.json();
+    }
+
+    geetAllBooks = async () => {
+        const res = await this.getResource(`/books/`);
+        return res.map(this._transformBook);
+    }
+
+    getBook = async (id) => {
+        const book = await this.getResource(`/books/${id}/`);
+        return this._transformBook(book);
+    }
+
+    geetAllCharacters = async () => {
+        const res = await this.getResource(`/characters?page=5&pageSize=10`);
+        return res.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const character = await this.getResource(`/characters/${id}`);
+        return this._transformCharacter(character);
+    }
+
+    geetAllHouses = async () => {
+        const res = await this.getResource(`/houses/`);
+        return res.map(this._transformHouse);
+    }
+
+    getHouse = async (id) => {
+        const house = await this.getResource(`/houses/${id}/`);
+        return this._transformHouse(house);
+    }
+
+    isSet(data) {
+        if (data) {
+            return data
+        } else {
+            return 'no data :('
+        }
+    }
+
+    _extractId = (item) => {
+        const idRegExp = /\/([0-9]*)$/;
+        return item.url.match(idRegExp)[1];
+    }
+
+    
+}
